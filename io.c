12101bbe45e3b7cc06c000e2786cb125d590977e
Person <121923945+trashedsmiles@users.noreply.github.com>
2023-03-27 06:37:32 +0500

    Input Output Example

---
+++ io.c
@@ -0,0 +1,18 @@
+/*
+ Input and Output example
+   by Person
+   March 27, 2023
+*/
+
+#include<stdio.h>
+#define PI 3.14159
+
+int main () 
+{
+	double area = 0.0, radius = 0.0;
+	printf("Enter Radius bro: ");
+	scanf("%lf", &radius);
+	area = PI * radius * radius;
+	printf("radius of %lf meters; area is %lf sq, meters\n", radius, area);
+	return 0;
+}
