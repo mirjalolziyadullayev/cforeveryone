d0d6bf1a23d1b8e74b5a44f50593264077be5fda
Person <121923945+trashedsmiles@users.noreply.github.com>
2023-03-28 07:39:19 +0500

    Conversation of Fahrenheit to Celcius example

---
+++ fahrenheit.c
@@ -0,0 +1,17 @@
+/*
+ *Conversation of Fahrenheit to Celsius 
+ by Person and professor Ira Pohl
+ March 28, 2023
+ */
+#include <stdio.h>
+int main(void)
+{
+    int fahrenheit, celsius;
+    
+    printf("Please enter fahrenheit as an integer bro:");
+    scanf("%d", &fahrenheit);
+    celsius = (fahrenheit -32)/1.8; //note conversation
+    printf("\n %d fahrenheit is %d celsius.\n",
+		    fahrenheit, celsius);
+    return 0;
+}
