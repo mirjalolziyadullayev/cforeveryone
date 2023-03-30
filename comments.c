067e7bce52367b4ce5362f0b7c33bb12fd7f2070
Person <121923945+trashedsmiles@users.noreply.github.com>
2023-03-30 06:41:10 +0500

    Update comments.c

--- comments.c
+++ comments.c
@@ -8,10 +8,10 @@
 
 int main() 
 {
-  double area = 0.0, radius = 0.0; /* area in kilometers */
+  double area = 0.0, radius = 0.0; /* area in kilometers (this is the classic multi-line comment) */
   printf("Enter Radius bro: ");
   scanf("%lf", &radius);
-  area = PI * radius * radius; // classic formula
+  area = PI * radius * radius; // classic formula (this is the rest of line comment)
   printf("radius of %lf meters; area is %lf sq. meters\n", radius, area);
   return 0;
 }
