926df17dda00fa20b7a60af37c7e22cac977b0ef
Person <121923945+trashedsmiles@users.noreply.github.com>
2023-04-12 07:07:35 +0500

    Create while loop in C.c

---
+++ examples/while loop in C.c
@@ -0,0 +1,20 @@
+/*
+ *while loop in c (example)
+ *by Person
+ *Apr 12, 2023
+ */
+#include<stdio.h>
+
+int main()
+{
+  //local variable definition
+  int a = 10;
+  
+  //while loop execution
+  while (a < 20) {
+    printf("value os a: %d\n", a);
+    a++;
+  }
+  return 0;
+}
+  
